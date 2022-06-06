1f683734cd9dd62e389ee40cb967c5f294fbf1ce
Cyril H <76228887+cyrilovh@users.noreply.github.com>
2022-06-06 20:21:21 +0200

    btn edit on home + pictureList

--- src/controller/identityList.php
+++ src/controller/identityList.php
@@ -14,43 +14,43 @@
         }
     }
 
-    $resultPerPage = 3; // number max of result per page
+    /* SORT BY */
+    if(isset($_GET["ancestorOrderBy"])){ // if ancestorOrderBy is set
+    
+        $ancestorOrderBy = "lastUpdate";
+        if(in_array($_GET["ancestorOrderBy"], \enumList\ancestorOrderBy::names())){ // i check if the row for ancestorOrderBy is in the enumList
+            $ancestorOrderBy = $_GET["ancestorOrderBy"];
+        }
+
+        $sortBy = "ASC";
+        if(isset($_GET["sortBy"])){ // if ancestorOrderBy is set
+            if(in_array($_GET["sortBy"], \enumList\sortBy::names())){ // i check if VALUE is in the enumList
+                $sortBy = $_GET["sortBy"];
+            } 
+        }
+    }else{ // if ancestorOrderBy is not set
+        $ancestorOrderBy = "lastUpdate";
+        $sortBy = "ASC";
+    }
+
+
+    /* FILTER */
+    $filter = array();
+
+    if(isset($_GET["gender"])){ // if gender i check if value is in enumList
+        if(in_array($_GET["gender"], \enumList\gender::values())){
+            $filter["gender"] = $_GET["gender"];
+        }
+    }
+
+    $resultPerPage = 10; // number max of result per page
     $start = ($page-1)*$resultPerPage;
 
-    $ancestorCount = count(\model\ancestor::getList(array("id"))); // number of ancestor in DB
+    $ancestorCount = count(\model\ancestor::getList(array("id"), 0, NULL, array($ancestorOrderBy, $sortBy), $filter)); // number of ancestor in DB
     $pageCount = ceil($ancestorCount/$resultPerPage); // number of page
 
+
     if($ancestorCount > 0 && $page <= $pageCount){
-        
-        /* SORT BY */
-        if(isset($_GET["ancestorOrderBy"])){ // if ancestorOrderBy is set
-            
-            $ancestorOrderBy = "lastUpdate";
-            if(in_array($_GET["ancestorOrderBy"], \enumList\ancestorOrderBy::names())){ // i check if the row for ancestorOrderBy is in the enumList
-                $ancestorOrderBy = $_GET["ancestorOrderBy"];
-            }
-
-            $sortBy = "ASC";
-            if(isset($_GET["sortBy"])){ // if ancestorOrderBy is set
-                if(in_array($_GET["sortBy"], \enumList\sortBy::names())){ // i check if VALUE is in the enumList
-                    $sortBy = $_GET["sortBy"];
-                } 
-            }
-        }else{ // if ancestorOrderBy is not set
-            $ancestorOrderBy = "lastUpdate";
-            $sortBy = "ASC";
-        }
-
-
-        /* FILTER */
-        $filter = array();
-
-        if(isset($_GET["gender"])){ // if gender i check if value is in enumList
-            if(in_array($_GET["gender"], \enumList\gender::values())){
-                $filter["gender"] = $_GET["gender"];
-            }
-        }
-
         $ancestorList = \model\ancestor::getList(array("id", "firstNameList", "lastName", "photo",  "maidenName", "gender", "birthDay"), $start, $resultPerPage, array($ancestorOrderBy, $sortBy), $filter);
         mcv::addView("identityList");
     }else{ // if any identity card or any result with the filters
--- src/view/pictureList.php
+++ src/view/pictureList.php
@@ -1,7 +1,6 @@
 <aside>
     <h2>Photos de famille:</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h3>Tous les albums</h3>
+    <p><?=$gng_paramList->get("pictureListSummary"); ?> <span class="btn btn-outline-info btn-sm"><i class="fa-solid fa-pen"></i> &Eacute;diter</span></p>
     <!-- START LIST -->
     <div class="pictureList">
         <div class="label">
@@ -59,73 +58,21 @@
                 </div>
             </div>
         </div>
+
+        <div class="label">
+            <div class="subLabel">
+                <img class="thumb"src="/assets/img/defaultPicture_480.webp" alt="" title=""/>
+            </div>
+            <div class="subLabel detail">
+                <div>
+                    <a class="title" href="/displayArchive/?id=35">Photo de famille Dufour</a>
+                </div>
+                <div>
+                    <p>Courte description de l'album</p>
+                </div>
+            </div>
+        </div>
+        
     </div>
     <!-- END LIST -->
-
-    <h3>Photos non-classées:</h3>
-    <!-- END LIST -->
-    <div class="pictureList">
-        <div class="label">
-            <div class="subLabel">
-                <img class="thumb"src="/assets/img/defaultPicture_480.webp" alt="" title=""/>
-            </div>
-            <div class="subLabel detail">
-                <div>
-                    <a class="title" href="/displayArchive/?id=35">Photo de famille</a>
-                </div>
-                <div>
-                    <p>Courte description de la photo.</p>
-                    <p><small><i class="fa-solid fa-location-dot"></i> Paris</small></p>
-                    <p><small><i class="fa-solid fa-calendar-days"></i> 1926</small></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="label">
-            <div class="subLabel">
-                <img class="thumb"src="/assets/img/defaultPicture_480.webp" alt="" title=""/>
-            </div>
-            <div class="subLabel detail">
-                <div>
-                    <a class="title" href="/displayArchive/?id=35">Photo de famille</a>
-                </div>
-                <div>
-                    <p>Courte description de la photo</p>
-                    <p><small><i class="fa-solid fa-link"></i> example.org</small></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="label">
-            <div class="subLabel">
-                <img class="thumb"src="/assets/img/defaultPicture_480.webp" alt="" title=""/>
-            </div>
-            <div class="subLabel detail">
-                <div>
-                    <a class="title" href="/displayArchive/?id=35">Photo de famille</a>
-                </div>
-                <div>
-                    <p>Courte description de la photo</p>
-                    <p><small><i class="fa-solid fa-link"></i> example.org</small></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="label">
-            <div class="subLabel">
-                <img class="thumb"src="/assets/img/defaultPicture_480.webp" alt="" title=""/>
-            </div>
-            <div class="subLabel detail">
-                <div>
-                    <a class="title" href="/displayArchive/?id=35">Photo de famille</a>
-                </div>
-                <div>
-                    <p>Courte description de la photo</p>
-                    <p><small><i class="fa-solid fa-link"></i> example.org</small></p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- END LIST -->
-    <div class="txt-right bold"><a href='/fullPictureList' class="btn btn-primary">Afficher toutes les photos &rarr;</a></div>
 </aside>
