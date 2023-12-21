c4c3fb2014ab9704e690eb25499ae0819aef7970
Cyril H <76228887+cyrilovh@users.noreply.github.com>
2023-12-21 21:47:33 +0100

    dry

--- src/view/signup.php
+++ src/view/signup.php
@@ -3,6 +3,6 @@
     <h2><?=$title; ?></h2>
     <?=isset($msgSuccess) ? die("<div class='alert alert-success'>".$msgSuccess."</div>") : ""; ?> <!-- success message -->
     <?=(isset($msgError)) ? "<div class='alert alert-danger'>".$msgError."</div>" : ""; ?> <!-- form errors -->
-    <?=(isset($errorList)) ? (!empty($errorList) ? "<div class='alert alert-danger'>b: ".implode("<br>",$errorList)."</div>" : "") : ""; ?> <!-- form errors -->
+    <?=(isset($errorList)) ? (!empty($errorList) ? "<div class='alert alert-danger'>".implode("<br>",$errorList)."</div>" : "") : ""; ?> <!-- form errors -->
     <?=$form->display(); ?>
 </aside>
--- src/view/userForm.php
+++ src/view/userForm.php
@@ -2,9 +2,9 @@
 <aside>
     <h2><?=$title; ?></h2>
     <?=isset($text) ? $text : ""; ?>
-    <?=isset($msgSuccess) ? die("<div class='alert alert-success'>".$msgSuccess."</div>") : ""; ?> <!-- success message -->
-    <?=(isset($msgError)) ? die("<div class='alert alert-danger'>".$msgError)."</div>" : ""; ?> <!-- form errors -->
-    <?=(isset($errorList)) ? die("<div class='alert alert-danger'>".implode("<br>",$errorList)."</div>") : ""; ?> <!-- form errors -->
+    <?=isset($msgSuccess) ? die("<div class='alert alert-success'>".$msgSuccess."</div>") : ""; ?> <!-- success message : this method will be deprecated (will be replaced by poo method) -->
+    <?=(isset($msgError)) ? die("<div class='alert alert-danger'>".$msgError)."</div>" : ""; ?> <!-- form errors : this method will be deprecated (will be replaced by poo method) -->
+    <?=(isset($errorList)) ? die("<div class='alert alert-danger'>".implode("<br>",$errorList)."</div>") : ""; ?> <!-- form errors : this method will be deprecated (will be replaced by poo method) -->
     <?=($messageList->isEmpty()) ? "" : die($messageList->getAllHTML()); ?> <!-- form errors (poo method) -->
     <?=isset($form) ? $form->display() : "" ?>
 </aside>
